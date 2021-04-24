bed0405603c7429ea94a02675858df3a0c7695b6
Alga <algavania454@gmail.com>
2021-04-24 14:26:13 +0700

    Initial commit

---
+++ app/src/main/java/com/upgrading/secondupgrading/DetailActivity.java
@@ -0,0 +1,33 @@
+package com.upgrading.secondupgrading;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        TextView tv_name = findViewById(R.id.tv_name_detail);
+        TextView tv_desc = findViewById(R.id.tv_desc_detail);
+        ImageView img_team = findViewById(R.id.img_team_detail);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            String name = bundle.getString("name");
+            String desc = bundle.getString("desc");
+            String image = bundle.getString("image");
+
+            tv_name.setText(name);
+            tv_desc.setText(desc);
+            Picasso.get().load(image).placeholder(R.mipmap.ic_launcher).error(R.mipmap.ic_launcher_round).into(img_team);
+        }
+    }
+}
---
+++ app/src/main/java/com/upgrading/secondupgrading/MainActivity.java
@@ -0,0 +1,85 @@
+package com.upgrading.secondupgrading;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.androidnetworking.AndroidNetworking;
+import com.androidnetworking.error.ANError;
+import com.androidnetworking.interfaces.JSONObjectRequestListener;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String BASE_URL = "https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?s=Soccer&c=Spain";
+    private TeamAdapter adapter;
+    private ArrayList<TeamModel> arrayList;
+    private RecyclerView recyclerView;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.rv_main);
+        progressBar = findViewById(R.id.progress_bar);
+
+        addData();
+    }
+
+    private void addData() {
+        AndroidNetworking.get(BASE_URL)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            arrayList = new ArrayList<>();
+                            JSONArray teamsArray = response.getJSONArray("teams");
+                            for (int i = 0; i < teamsArray.length(); i++) {
+                                JSONObject teamObject = teamsArray.getJSONObject(i);
+                                String name = teamObject.getString("strTeam");
+                                String description = teamObject.getString("strDescriptionEN");
+                                String image = teamObject.getString("strTeamBadge");
+                                arrayList.add(new TeamModel(image, name, description));
+                            }
+                            adapter = new TeamAdapter(getApplicationContext(), arrayList);
+                            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+                            recyclerView.setLayoutManager(layoutManager);
+                            recyclerView.setAdapter(adapter);
+
+                            adapter.setOnItemClickListener(new TeamAdapter.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(int position) {
+                                    Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+                                    intent.putExtra("name", arrayList.get(position).getTeamName());
+                                    intent.putExtra("desc", arrayList.get(position).getTeamDesc());
+                                    intent.putExtra("image", arrayList.get(position).getTeamImage());
+                                    startActivity(intent);
+                                }
+                            });
+
+                            progressBar.setVisibility(View.GONE);
+                        } catch (Exception e) {
+                            Log.d("error", e.toString());
+                        }
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+                        Log.d("error", anError.toString());
+                    }
+                });
+    }
+}
